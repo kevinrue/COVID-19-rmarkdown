e83594d44e3c354dd9041546393ea712e94cd928
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-05-21 07:00:13 +0100

    print diagnostics in build script

--- _build.sh
+++ _build.sh
@@ -1,7 +1,16 @@
+pwd
 cd COVID-19
+pwd
+git status
 git pull
+git status
 cd ..
+pwd
+git status
 git add COVID-19
+git status
 git commit -m "update data"
+git status
 git push
+git status
 Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
